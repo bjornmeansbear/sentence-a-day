a340cae47ab2c6e9dd15f4c25f1ab9a08031b8b6
Kristian Bjornard <bjornmeansbear@pm.me>
2021-07-10 15:30:20 -0400

    trying to get some build static stuff working

---
+++ build.sh
@@ -0,0 +1,3 @@
+tiddlywiki sad2021tw --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain
+tiddlywiki sad2021tw --rendertiddler $:/core/templates/static.template.html static.html text/plain
+tiddlywiki sad2021tw --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
