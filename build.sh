94d4d02c2af908f544f87c23f300cfe7b77db6bc
Kristian Bjornard <bjornmeansbear@pm.me>
2022-02-03 01:21:26 -0500

    adding so much stuff, maybe a new beginning this february?

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 tiddlywiki sad2021tw --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain
 tiddlywiki sad2021tw --rendertiddler $:/core/templates/static.template.html static.html text/plain
 tiddlywiki sad2021tw --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
+
+# https://www.didaxy.com/exporting-static-sites-from-tiddlywiki-part-1
+tiddlywiki --rendertiddlers [!is[system]] $:/core/templates/static.tiddler.html static text/plain --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
