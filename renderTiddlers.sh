da4fcf06df964dee324ac220545eded46c154f80
Kristian Bjornard <bjornmeansbear@pm.me>
2023-02-28 23:32:06 -0500

    trying a few new things?

--- renderTiddlers.sh
+++ renderTiddlers.sh
@@ -1,3 +1,4 @@
 tiddlywiki sad2021tw --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain
 tiddlywiki sad2021tw --rendertiddler $:/core/templates/static.template.html static.html text/plain
 tiddlywiki sad2021tw --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
+tiddlywiki sad2021tw --rendertiddler $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain
