af76508bdc36e9db9f9056b47c4d008266e7e6ab
Kristian Bjornard <bjornmeansbear@pm.me>
2024-01-05 02:35:26 -0500

    Its feeling pretty good to be adding to this again

--- build.sh
+++ build.sh
@@ -3,4 +3,8 @@
 # tiddlywiki sad2021tw --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
 
 # https://www.didaxy.com/exporting-static-sites-from-tiddlywiki-part-1
-tiddlywiki sad2021tw --rendertiddlers [!is[system]] $:/core/templates/static.tiddler.html static text/plain --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
+# tiddlywiki sad2021tw --rendertiddlers [!is[system]] $:/core/templates/static.tiddler.html static text/plain --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
+
+# tiddlywiki sad2021tw --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain --rendertiddler $:/core/templates/static.template.html static.html text/plain --rendertiddler $:/core/templates/static.template.css static/static.css text/plain --rendertiddler $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain
+
+tiddlywiki sad2021tw --render '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain --render $:/core/templates/static.template.html static.html text/plain --render $:/core/templates/static.template.css static/static.css text/plain --render $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain --build index
