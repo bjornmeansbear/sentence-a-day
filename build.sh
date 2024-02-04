333ab3868f3351fe60445f3c0b725e5625b3e166
Kristian Bjornard <bjornmeansbear@pm.me>
2024-02-04 00:09:19 -0500

    trying to learn the new render stuff

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@
 
 # tiddlywiki sad2021tw --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain --rendertiddler $:/core/templates/static.template.html static.html text/plain --rendertiddler $:/core/templates/static.template.css static/static.css text/plain --rendertiddler $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain
 
+#Okay: this renders all the static files AND does and index.html with some dyanmism?
 tiddlywiki sad2021tw --render '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain --render $:/core/templates/static.template.html static.html text/plain --render $:/core/templates/static.template.css static/static.css text/plain --render $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain --build index
