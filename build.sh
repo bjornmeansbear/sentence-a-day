b09edf43935fcd1df9bdd638d777c20f7a6e04ba
Kristian Bjornard <bjornmeansbear@pm.me>
2025-03-23 23:22:27 -0400

    apparently I added a bunch of things a long time ago and didnt commit!?

--- build.sh
+++ build.sh
@@ -8,4 +8,9 @@
 # tiddlywiki sad2021tw --rendertiddlers '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain --rendertiddler $:/core/templates/static.template.html static.html text/plain --rendertiddler $:/core/templates/static.template.css static/static.css text/plain --rendertiddler $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain
 
 #Okay: this renders all the static files AND does and index.html with some dyanmism?
-tiddlywiki sad2021tw --render '[!is[system]]' $:/core/templates/static.tiddler.html static text/plain --render $:/core/templates/static.template.html static.html text/plain --render $:/core/templates/static.template.css static/static.css text/plain --render $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain --build index
+#tiddlywiki sad2021tw --render $:/core/templates/static.template.css static/static.css text/plain
+#tiddlywiki sad2021tw --render '[!is[system]]' '[encodeuricomponent[]addprefix[tiddlers/]addsuffix[.html]]'
+tiddlywiki sad2021tw --render '[!is[system]]' '[encodeuricomponent[]addprefix[static/]addsuffix[.html]]' 'text/plain' '$:/core/templates/static.tiddler.html'
+tiddlywiki sad2021tw --render $:/core/templates/static.template.html static.html text/plain
+tiddlywiki sad2021tw --render $:/core/templates/alltiddlers.template.html alltiddlers.html text/plain
+tiddlywiki sad2021tw --build index
